ba6bc862d3edc27c443240d41308eef34adb15b5
Schoelje <schoelje@hotmail.nl>
2013-02-02 12:32:48 +0100

    Files needed for branch merge

--- usr/lib/ddm/main.py
+++ usr/lib/ddm/main.py
@@ -20,7 +20,7 @@ def usage():
         if hwOpt != '':
             hwOpt += ', '
         hwOpt += hw
-    hlp = """Usage: debian-driver-manager [options]
+    hlp = """Usage: device-driver-manager [options]
 
 Options:
   -c (--codes): comma separated list with pre-selected hardware
